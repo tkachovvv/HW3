a428ceef40e81ff360faf2edf60bac90e362550c
tkachovvv <dmitiy.tk.skyeng@outlook.com>
2022-10-22 21:04:50 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,52 @@ public class Main {
         System.out.println(boysScoreAtSchool);
         double girlsScoreAtSchool = 9.76;
         System.out.println(girlsScoreAtSchool);
+        float reaction = 27.12f;
+        System.out.println(reaction);
+        long rally = 987678965549L;
+        System.out.println(rally);
+        double angerLevel = 2.786;
+        System.out.println(angerLevel);
+        boolean realRally = rally >=9876789655492L;
+        System.out.println(realRally);
+        short oranges = 569;
+        System.out.println(oranges);
+        short temperatureInSiberia = -159;
+        System.out.println(temperatureInSiberia);
+        int peopleInVillage = 27897;
+        System.out.println(peopleInVillage);
+        byte numberOfParticipants = 67;
+        System.out.println(numberOfParticipants);
+        short apples = 517;
+        System.out.println(apples);
+        int cars = -1474836682;
+        System.out.println(cars);
+        float carpenter = 2.575f;
+        System.out.println(carpenter);
+        byte pressure = -57;
+        System.out.println(pressure);
+        short bananas = 28151;
+        System.out.println(bananas);
+        short pork = -32768;
+        System.out.println(pork);
+        int peopleInLasVegas = 2000000;
+        System.out.println(peopleInLasVegas);
+        long infinity = 999999999L;
+        System.out.println(infinity);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 }
