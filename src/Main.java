c98a2b851e181d66eadce8f8f6eba17977c1ba6a
tkachovvv <dmitiy.tk.skyeng@outlook.com>
2022-10-22 20:06:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int boyfriend = 21;
+        System.out.println (boyfriend);
+        byte girlfriend = 35;
+        System.out.println (girlfriend);
+        short boyfriendSalary = 32000;
+        long girlfriendSalary = 55000;
+        System.out.println(girlfriendSalary);
+        System.out.println(boyfriendSalary);
+        float boysScoreAtSchool = 3.34f;
+        System.out.println(boysScoreAtSchool);
+        double girlsScoreAtSchool = 9.76;
+        System.out.println(girlsScoreAtSchool);
+
+
+
+    }
+
+}
